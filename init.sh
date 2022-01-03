8d18121292d7d07720acc229d88467f60f6b97b0
tombetthauser <tombetthauser@gmail.com>
2022-01-02 23:28:33 -0800

    Get init working with up and down scripts

--- init.sh
+++ init.sh
@@ -1,21 +1,8 @@
 #! /bin/bash
 
-end=$((SECONDS+30))
-arr=(hello bootay erin sexy pants ohyeah sweet juicy yum nice hottness fire spicy extra)
-
-randArrayElement(){ arr=("${!1}"); say ${arr["$[RANDOM % ${#arr[@]}]"]}; }
-
-open http://unknowablesymbols.com -a /Applications/Google\ Chrome.app/
-
-open ~/Music/slow_mixtape/06-rain.mp3 -a /Applications/VLC.app/
-
-while [ $SECONDS -lt $end ]; do
-  randArrayElement "arr[@]"
-  sleep 1
-  :
+while :
+do
+  bash ./script/up.sh
+  bash ./script/down.sh
+  sleep 3
 done
-
-killall Google\ Chrome
-killall VLC
-
-sleep 3
---
+++ script/up.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+end=$((SECONDS+30))
+arr=(hello bootay erin sexy pants ohyeah sweet juicy yum nice hottness fire spicy extra)
+
+randArrayElement(){ arr=("${!1}"); say ${arr["$[RANDOM % ${#arr[@]}]"]}; }
+
+open http://unknowablesymbols.com -a /Applications/Google\ Chrome.app/
+
+open ~/Music/slow_mixtape/06-rain.mp3 -a /Applications/VLC.app/
+
+while [ $SECONDS -lt $end ]; do
+  randArrayElement "arr[@]"
+  sleep 1
+  :
+done
