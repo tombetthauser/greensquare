f1deaf4346403eb0a030086b40f1b8773dbb1319
tombetthauser <tombetthauser@gmail.com>
2022-01-03 00:31:53 -0800

    Add random playback to youtube videos

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,16 +1,26 @@
 #! /bin/bash
 
-end=$((SECONDS+15))
-arr=(hello bootay erin sexy pants ohyeah sweet juicy yum nice hottness fire spicy extra)
+# Open a youtube video and pause to allow chrome to start autoplaying at random 30sec starting point...
+open "https://www.youtube.com/watch?v=9sJUDx7iEJw&t=$[$RANDOM % 30]s" -a /Applications/Google\ Chrome.app/
+sleep 10
+open "https://www.youtube.com/watch?v=JY-ajPNQMh0&t=$[$RANDOM % 30]s" -a /Applications/Google\ Chrome.app/
+sleep 10
 
-randArrayElement(){ arr=("${!1}"); say ${arr["$[RANDOM % ${#arr[@]}]"]}; }
-
+# Open a new chrome tab with a visual project...
 open http://unknowablesymbols.com -a /Applications/Google\ Chrome.app/
 
-open ~/Music/slow_mixtape/06-rain.mp3 -a /Applications/VLC.app/
+# Randomly chop an audio file to make a random starting point then play it in VLC...
+# ffmpeg -ss 10 -t 6 -i ./assets/audio/rain.mp3 ./scripts/temp/output.mp3
+# open ./scripts/temp/output.mp3 -a /Applications/VLC.app/
 
+# Create array of words and start saying them randomly at regular time interval...
+#arr=(hello bootay erin sexy pants ohyeah sweet juicy yum nice hottness fire spicy extra)
+#randArrayElement(){ arr=("${!1}"); say ${arr["$[RANDOM % ${#arr[@]}]"]}; }
+
+# Set time before up script ends and init script resets it...
+end=$((SECONDS+45))
 while [ $SECONDS -lt $end ]; do
-  randArrayElement "arr[@]"
-  sleep 1
-  :
+#  randArrayElement "arr[@]"
+#  sleep 1
+ :
 done
